0809c444783475153152ed3e50e056d0e9664c39
maneashish <ashishmane4991@gmail.com>
2024-02-26 06:11:35 +0530

    second

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -5,12 +5,8 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [RouterTestingModule],
+      declarations: [AppComponent],
     }).compileComponents();
   }));
 
@@ -20,16 +16,18 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'andresjosehr-portfolio'`, () => {
+  it(`should have as title 'Ashishmane-portfolio'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('andresjosehr-portfolio');
+    expect(app.title).toEqual('Ashishmane-portfolio');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('andresjosehr-portfolio app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain(
+      'Ashishmane-portfolio app is running!'
+    );
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,40 +2,39 @@ import { Component, OnInit } from '@angular/core';
 import * as AOS from 'aos';
 import { Title, Meta } from '@angular/platform-browser';
 import { TranslateService } from '@ngx-translate/core';
-import {LanguageService} from "src/app/services/language/language.service"
+import { LanguageService } from 'src/app/services/language/language.service';
 import { Location } from '@angular/common';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit{
-  title = 'andresjosehr-portfolio';
-  
+export class AppComponent implements OnInit {
+  title = 'Ashishmane-portfolio';
+
   constructor(
     private titleService: Title,
     private metaService: Meta,
     private translateService: TranslateService,
     private location: Location,
     private languageService: LanguageService
-    ){
-    }
-  ngOnInit(): void{
-    
-    this.languageService.initLanguage()
+  ) {}
+  ngOnInit(): void {
+    this.languageService.initLanguage();
 
-
-    this.titleService.setTitle( "Ashish Mane | Software Engineer" );
+    this.titleService.setTitle('Ashish Mane | Software Engineer');
 
     this.metaService.addTags([
-      {name: 'keywords', content: 'Frontend, software, developer'},
-      {name: 'description', content: 'Con 4 años de experiencia desarrollando sistemas, interfaces, bots y soluciones tecnológicas  para hacer de la web un lugar mejor. En mi trabajo me gusta liderar, proponer y ejecutar ideas, escribir y refactorizar código limpio, reutilizable y escalable.'},
+      { name: 'keywords', content: 'Frontend, software, developer' },
+      {
+        name: 'description',
+        content:
+          'Con 4 años de experiencia desarrollando sistemas, interfaces, bots y soluciones tecnológicas  para hacer de la web un lugar mejor. En mi trabajo me gusta liderar, proponer y ejecutar ideas, escribir y refactorizar código limpio, reutilizable y escalable.',
+      },
     ]);
-    
-    
-    AOS.init(); 
 
+    AOS.init();
   }
 }
