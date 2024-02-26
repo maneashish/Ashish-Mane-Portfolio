94229a900b4bc2c242a4a3e542f5963cd8d0cf5a
maneashish <ashishmane4991@gmail.com>
2024-02-26 13:27:25 +0530

    commit

--- src/app/components/home/about/about.component.ts
+++ src/app/components/home/about/about.component.ts
@@ -7,7 +7,42 @@ import { AnalyticsService } from 'src/app/services/analytics/analytics.service';
   styleUrls: ['./about.component.scss']
 })
 export class AboutComponent implements OnInit {
-
+  technologies = [
+    "Angular",
+    "Node.js",
+    "CSS",
+    "Bootstrap",
+    "jQuery",
+    "MongoDB",
+    "HTML5",
+    "CSS3",
+    "JavaScript",
+    "TypeScript",
+    "Kendo UI",
+    "Responsive design",
+    "Cross-browser compatibility",
+    "MS Office suite",
+    "Visual Studio",
+    "Sublime Text Editor",
+    "Photoshop",
+    "Postman",
+    "Unit Testing (Karma, Jasmine)",
+    "Integration Testing",
+    "End-to-End Testing",
+    "Git",
+    "Bitbucket",
+    "Sourcetree",
+    "GitHub",
+    "Agile methodologies",
+    "Jira",
+    "RESTful APIs",
+    "JSON",
+    "MVC design pattern",
+    "Single Page Applications (SPAs)",
+    "Project management",
+    "Problem-solving skills",
+    "Strong communication skills"
+  ];
   constructor(
     public analyticsService: AnalyticsService
   ) { }
